080c8c167f5f0f0fe1e21597f3206b9163ec337c
Alex K <qirex93@gmail.com>
2021-03-08 18:57:46 +0300

    react init

---
+++ my-react-project/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App = () => {
+    return (
+      <div className="App">
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+      </div> 
+  );
+ }
+
+export default App;
