61d0ae374436d331f3025202e40f81ff6bd1ce8f
Alex K <qirex93@gmail.com>
2021-03-10 23:37:06 +0300

    11 lesson

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import './App.css'
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://free-images.com/or/8a4e/lion_head_isolated_art.jpg' />
+      </header>
+      <nav className='nav'>
+        <div>
+          Profile
+        </div>
+        <div>
+          Messages
+        </div>
+      </nav>
+
+      <div className='content'>
+
+        <div>
+          <img src='https://photoshop-kopona.com/uploads/posts/2018-03/1521289405_17-8.jpg' />
+        </div>
+
+        <div>
+          <img src='https://pixelbox.ru/wp-content/uploads/2020/12/ava-vk-cats-90.jpg' />
+        </div>
+
+        <div>
+          My posts
+        </div>
+
+        <div>
+          New post
+        </div>
+
+        <div>
+          post 1
+        </div>
+
+        <div>
+          post 2
+        </div>
+
+      </div>
+
+      <footer>
+
+      </footer>
+
     </div>
   );
 }
