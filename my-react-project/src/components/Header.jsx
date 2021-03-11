f1906ba7ba4b19935244ff31f7742acd11b767a6
Alex K <qirex93@gmail.com>
2021-03-11 15:35:36 +0300

    react-lesson 12

---
+++ my-react-project/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://free-images.com/or/8a4e/lion_head_isolated_art.jpg' />
+      </header>
+}
+
+export default Header;
---
+++ my-react-project/src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+
+    <div>
+      <img className='bg' src='https://photoshop-kopona.com/uploads/posts/2018-03/1521289405_17-8.jpg' />
+    </div>
+
+    <div>
+      <img className='avatar' src='https://pixelbox.ru/wp-content/uploads/2020/12/ava-vk-cats-90.jpg' />
+    </div>
+
+    <div>
+      My posts
+    </div>
+
+    <div>
+      New post
+    </div>
+
+    <div>
+      post 1
+    </div>
+
+    <div>
+      post 2
+    </div>
+
+  </div>
+}
+
+export default Profile;
